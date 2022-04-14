b3b63d2831ea6bac22bf3eac397192cd16847262
Tony Xiao <tonyx.ca@gmail.com>
2022-04-14 13:24:17 +0800

    Install react navigation

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
+import { NavigationContainer } from '@react-navigation/native'
 import 'expo-dev-client'
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app! Inside nav container</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +21,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
