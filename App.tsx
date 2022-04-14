cf65e5068e8e35f00b72ee9071bc1e4e8f59c3fd
Tony Xiao <tonyx.ca@gmail.com>
2022-04-14 14:25:56 +0800

    Playing with getId

--- App.tsx
+++ App.tsx
@@ -12,49 +12,82 @@ import {
   NativeStackScreenProps,
 } from '@react-navigation/native-stack'
 
-function HomeScreen(props: NativeStackScreenProps<RootParamList, 'Home'>) {
-  console.log(props)
+function Links() {
+  const nav = useNavigation()
+  ;(window as any).nav = nav
+  return (
+    <View style={{ marginTop: 30 }}>
+      <Link to={{ screen: 'Tabs' }}>Go to tabs screen</Link>
+      <Link to={{ screen: 'Settings' }}>Go to settings screen</Link>
+      <Link to={{ screen: 'TabDetail', params: { tabId: '1' } }}>
+        Go to tab 1
+      </Link>
+      <Link to={{ screen: 'TabDetail', params: { tabId: '2' } }}>
+        Go to tab 2
+      </Link>
+    </View>
+  )
+}
+
+function TabsScreen(props: NativeStackScreenProps<RootParamList, 'Tabs'>) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Link to={{ screen: 'Settings' }}>Go to settings screen</Link>
+      <Text>Tabs Screen</Text>
+      <Links />
+    </View>
+  )
+}
+
+function TabDetailScreen(
+  props: NativeStackScreenProps<RootParamList, 'TabDetail'>
+) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Tabs detail: {props.route.params.tabId}</Text>
+      <Links />
     </View>
   )
 }
 
 function SettingsScreen() {
-  const nav = useNavigation()
-  ;(window as any).nav = nav
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Settings Screen</Text>
+      <Links />
     </View>
   )
 }
 
-const Stack = createNativeStackNavigator()
-
 export type RootParamList = {
-  Home: { bookId: string }
+  Tabs: {}
+  TabDetail: { tabId: string }
   Settings: {}
 }
 
 const linking: LinkingOptions<RootParamList> = {
   prefixes: [Linking.createURL('/')],
   config: {
-    initialRouteName: 'Home',
+    initialRouteName: 'Tabs',
     screens: {
-      Home: ':bookId',
+      Tabs: 'tabs',
+      TabDetail: 'tabs/:tabId',
       Settings: 'settings',
     },
   },
 }
 
+const Stack = createNativeStackNavigator<RootParamList>()
+
 export default function App() {
   return (
     <NavigationContainer linking={linking}>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Tabs" component={TabsScreen} />
+        <Stack.Screen
+          name="TabDetail"
+          component={TabDetailScreen}
+          getId={({ params }) => params.tabId}
+        />
         <Stack.Screen name="Settings" component={SettingsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
